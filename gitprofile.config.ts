d5645d804e7c5d830748bcbc84f71db6f05fedff
Rebecca Hartway <111208134+rhartway@users.noreply.github.com>
2024-10-31 13:45:07 -0400

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -83,8 +83,8 @@ const CONFIG = {
       'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
-    'PHP',
-    'Laravel',
+    'Python',
+    'Java',
     'JavaScript',
     'React.js',
     'Node.js',
